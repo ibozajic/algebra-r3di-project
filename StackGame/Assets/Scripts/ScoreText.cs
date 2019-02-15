971162b3d9f728ac83e215a843c3ccce3a8f97f4
Ivan Bozajic <ivan.bozajic@gmail.com>
2019-02-15 16:52:43 +0100

    First commit

---
+++ StackGame/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    private int score;
+    private TextMeshProUGUI[] texts;
+    private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        texts = GetComponents<TextMeshProUGUI>();
+        Debug.Log("Texts Lengts: ");
+        GameManager.OnCubeSpawned += GameManager_OnCubeSawned;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnCubeSpawned -= GameManager_OnCubeSawned;
+    }
+
+    private void GameManager_OnCubeSawned()
+    {
+        score++;
+        text.text = "Score: " + score;
+    }
+}
