658f2e51798e7ec44a910e50ac9f3089e0ba03b9
Ivan Bozajic <ivan.bozajic@gmail.com>
2019-02-15 20:31:23 +0100

    Add Music volumen control in Option menu

--- StackGame/Assets/Scripts/GameManager.cs
+++ StackGame/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,12 +10,35 @@ public class GameManager : MonoBehaviour
     public static event Action OnCubeSpawned = delegate { };
 
     private CubeSpawner[] spawners;
+
+    public AudioSource gameMusic;
+
     private int spawnerIndex;
     private CubeSpawner currentSpawner;
 
     private void Awake()
     {
         spawners = FindObjectsOfType<CubeSpawner>();
+        gameMusic = GameObject.Find("GameMusic").GetComponent<AudioSource>();
+
+        string path = Application.persistentDataPath + "/game.dat";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+            stream.Close();
+
+            gameMusic.volume = data.musicVolumen;
+            Debug.Log("Retrive saved volumen: " + data.musicVolumen);
+        }
+        else
+        {
+            gameMusic.volume = 0.01f;
+            Debug.Log("Save file not find in " + path);
+        }
     }
 
     private void Update()
--- StackGame/Assets/Scripts/MainMenu.cs
+++ StackGame/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider volumSlider;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +18,42 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ChangeMusicVolume()
+    {
+        volumSlider = FindObjectOfType<Slider>();        
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/game.dat";
+
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData data = new GameData(volumSlider.value);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+
+        Debug.Log("Volume saved: " + volumSlider.value);
+    }
+
+    private void Awake()
+    {
+        string path = Application.persistentDataPath + "/game.dat";
+
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+            stream.Close();
+
+            Debug.Log("Retrive saved volumen: " + data.musicVolumen);
+        }
+        else
+        {
+            Debug.Log("Save file not find in " + path);
+        }
     }
 }
